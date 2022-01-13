8c9f5d4d62554d441a1d4085fcdebd3c01cfc491
Bruno Bauermann <bgbauermann@gmail.com>
2022-01-12 20:57:37 -0500

    Add Bulma css styling

---
+++ static/layout.js
@@ -0,0 +1,8 @@
+// mobile navbar
+
+const burgerIcon = document.querySelector('.navbar-burger');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active')
+})
