d18bac47dec34186aac4d8d6352c506400ca616c
Bruno Bauermann <bgbauermann@gmail.com>
2022-01-19 17:43:52 -0500

    Change application name for aws

--- wedding_app/__init__.py
+++ wedding_app/__init__.py
@@ -9,15 +9,15 @@ login_manager.login_view = 'rsvp'
 login_manager.login_message = ''
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
+    application = Flask(__name__)
+    application.config.from_object(Config)
 
     db.init_app(app)
     login_manager.init_app(app)
 
     from wedding_app.main.routes import main
     from wedding_app.guests.routes import guests
-    app.register_blueprint(main)
-    app.register_blueprint(guests)
+    application.register_blueprint(main)
+    application.register_blueprint(guests)
 
-    return app
+    return application
